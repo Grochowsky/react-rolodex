9df2df8b88585cc0677c4b131baebd988cac5960
grochowsky <grochowsky@Grochowsky.localdomain>
2023-02-02 21:27:34 +0100

    Filtered List

--- src/App.js
+++ src/App.js
@@ -9,26 +9,53 @@ class App extends Component {
 
     this.state = {
       monsters:[],
+      searchField:''
     }
+    console.log('Constructor')
   }
 
   componentDidMount() {
+    console.log('component did mount')
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then((users) => this.setState( () => {
+      console.log(users)
       return (
-        { monsters: users}
+        { monsters: users }
       )
     }, () => {
       console.log(this.state)
     }))
+    
   }
 
   render() {
+    console.log('Render')
+
+    const filteredMonsters = this.state.monsters.filter( (monster) => {
+      return monster.name.toLowerCase().includes(this.state.searchField)
+     });
+
+
     return (
       <div className="App">
+        <input 
+        className='search-box' 
+        type="search" 
+        placeholder='search' 
+        onChange={ (event) => {
+
+          console.log(event.target.value)
+          const searchField = event.target.value.toLowerCase();
+         
+
+          this.setState(() => {
+            return { searchField}
+          })
+        
+        }}/>
         {
-          this.state.monsters.map( (monster) => {
+          filteredMonsters.map( (monster) => {
             return <h1 key={monster.id}>{monster.name}</h1>
           })
         }
